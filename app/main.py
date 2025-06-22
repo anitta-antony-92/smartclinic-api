174acc5eb5eb0d23919d3031452af56ebf9834fd
anitta-antony-92 <anitta.antony.inbox@gmail.com>
2025-06-22 03:06:41 +0300

    🚀 Initial FastAPI project structure with root route

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.config import settings
-
+from app.routes import auth
 
 app = FastAPI(title=settings.APP_NAME)
 
+# Include routes
+app.include_router(auth.router)
+
+# Health check / root endpoint
 @app.get("/")
 def read_root():
     return {"message" : f"Welcome to {settings.APP_NAME}"}
